0bffe2755d5d8d6250fd1a17c8ff241f69986f28
Jit Mitra <development.jitmtr75@gmail.com>
2021-12-17 00:22:43 +0530

    building pokemon card

--- pokedex-app/src/components/pokemon/Pokemon.js
+++ pokedex-app/src/components/pokemon/Pokemon.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { type } from "@testing-library/user-event/dist/type";
+
+const TYPE_COLORS = {
+  bug: "B1C12E",
+  dark: "4F3A2D",
+  dragon: "755EDF",
+  electric: "FCBC17",
+  fairy: "F4B1F4",
+  fighting: "823551D",
+  fire: "E73B0C",
+  flying: "A3B3F7",
+  ghost: "6060B2",
+  grass: "74C236",
+  ground: "D3B357",
+  ice: "A3E7FD",
+  normal: "C8C4BC",
+  poison: "934594",
+  psychic: "ED4882",
+  rock: "B9A156",
+  steel: "B5B5C3",
+  water: "3295F6",
+};
 
 export default class Pokemon extends Component {
   state = {
@@ -167,12 +187,42 @@ export default class Pokemon extends Component {
                 <h5>{this.state.pokemonIndex}</h5>
               </div>
               <div className="col-7">
-                <div className="float-end">
-                  {this.state.types.map((type) => {
-                    <span key={type} className="badge badge-pill mr-1">
-                      {type}
-                    </span>;
-                  })}
+                <div className="float-right">
+                  {this.state.types.map((type) => (
+                    <span
+                      key={type}
+                      className="badge badge-primary badge-pill mr-1"
+                      style={{
+                        backgroundColor: `#${TYPE_COLORS[type]}`,
+                        color: "white",
+                      }}
+                    >
+                      {type
+                        .toLowerCase()
+                        .split(" ")
+                        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+                        .join(" ")}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="row align-items-center">
+                <div className="col-md-3">
+                  <img
+                    src={this.state.imageUrl}
+                    className="card-img-top rounded mx-auto mt-2"
+                  ></img>
+                </div>
+                <div className="col-md-9">
+                  <h4 className="mx-auto">
+                    {this.state.name
+                      .toLowerCase()
+                      .split(" ")
+                      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+                      .join(" ")}
+                  </h4>
                 </div>
               </div>
             </div>
